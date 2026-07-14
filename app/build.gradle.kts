c6e43ea1ebb1698c462e0beeb8128868cb20f152
halid <halid@MacBook-Pro-halid.local>
2026-07-14 23:04:51 +0300

    Треккинг

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "2.6.6"
+        versionName = "2.6.7"
 
         manifestPlaceholders["MAPS_API_KEY"] = localProperties.getProperty("MAPS_API_KEY", "")
 
